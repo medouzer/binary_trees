d0762c2d343b1969d47822cf8b607e937e8c0b82
ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-04 23:34:52 +0100

    all tasks without bitty

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes: function that counts the nodes with at least 1 child in a binary tree
+ * @tree: is the tree
+*/
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    size_t counter = 0;
+
+    if (tree == NULL)
+        return (0);
+    if (tree->left || tree->right)
+        counter = 1;
+    counter += binary_tree_nodes(tree->left);
+    counter += binary_tree_nodes(tree->right);
+    return (counter);
+}
--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,6 +3,7 @@
 /**
  * binary_tree_height: function that measures the height of a binary tree
  * @tree: is the tree
+ * Return: the height of the tree
 */
 
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -20,4 +21,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
         return (height_left + 1);
     else
         return (height_right + 1);
+}
+
+/**
+ * binary_tree_balance: function that measures the balance factor of a binary tree
+ * @tree: is the tree
+ * Return: return the balance factor of the tree
+*/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    int height_left = 0, height_right = 0;
+    if (tree == NULL)
+        return (0);
+    height_left = binary_tree_height(tree->left);
+    height_right = binary_tree_height(tree->right);
+    return (height_left - height_right);
 }
